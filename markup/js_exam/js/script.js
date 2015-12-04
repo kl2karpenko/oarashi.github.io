f7c7c2621f69b9908c36f7324cec232f28d1cd88
Igor <mac@macs-MacBook-Pro.local>
2015-12-04 08:44:00 +0200

    init

--- markup/js_exam/js/script.js
+++ markup/js_exam/js/script.js
@@ -1,99 +1,150 @@
-var game = {
-    startGame: function startGame() {
-        var gunman = new Gunman;
-        gunman.width =
+var Game = {
+    level: 0,
+    score: 0
 
-    },
-    nextLevel: function nextLevel() {},
-    restartGame: function restartGame() {},
-    score: 0,
-    gunmanRandomizer: function currentId() {}
+};
+
+var playerShoot = false;
+
+
+var Gunman = function (options) {
+
+
+    $('.button_restart').addClass('undisplay');
+    $('.button_next_level').addClass('undisplay');
+
+
+
+
+    function Timer() {
+        var timer = {
+            startTime: (new Date()).valueOf(),
+            currentTime: (new Date()).valueOf(),
+            inervalId: null,
+            interval: setInterval(function () {
+                timer.currentTime = (new Date).valueOf();
+            }, 100)
+        };
+
+        function MyDateInterval(interval) {
+            var time = {};
+            time.interval = interval;
+            time.s = Math.floor(interval / 1000) % 60 || 0;
+            time.ms = (Math.floor(interval / 100));
+            return time;
+        }
+
+
+        Object.observe(timer, function () {
+            var interval = new MyDateInterval(timer.currentTime - timer.startTime);
+            $('.player_timer').text(
+                interval.s + ' : ' + interval.ms
+            );
+        });
+
+
+
+    };
+
+    var currentGunman = options || 1;
+
+    this.move = function () {
+        $('#startGame').addClass('hide');
+        $('#credits').addClass('hide');
+        $('#gunman').addClass('gunman_' + currentGunman + '_walk');
+
+    };
+
+
+    this.stand = function () {
+        function gunmanStanding() {
+            $('#gunman').removeClass('gunman_' + currentGunman + '_walk');
+            $('#gunman').addClass('gunman_' + currentGunman + '_standing');
+            Timer();
+        }
+        $('#gunman').on('transitionend webkitTransitionEnd oTransitionEnd', gunmanStanding);
+
+    };
+
+    function shoot() {
+        $("#gunman").on('click', function () {
+            $('#gunman').removeClass('gunman_' + currentGunman + '_standing');
+            $('#gunman').addClass('gunman_' + currentGunman + '_falling');
+            $('.message').addClass('hide');
+            playerShoot = true;
+
+        });
+    };
+
+
+    this.fire = function () {
+        setTimeout(shoot, 5000);
+        setTimeout(function () {
+            $('.message').removeClass('hide');
+        }, 5500);
+        setTimeout(function () {
+            $('.button_next_level').removeClass('undisplay');
+        }, 6500);
+
+
+    };
+
+    function nextLevel() {
+        $('#gunman').removeClass('gunman_' + currentGunman + '_falling');
+        $('#gunman').removeClass('gunman_' + currentGunman + '_walking');
+        $('#gunman').css({
+            'left': 900 + 'px'
+        });
+
+        Game.level = Game.level++;
+        Game.score = Game.score + 1000;
+        gunman.move();
+        gunman.stand();
+        gunman.fire();
+    }
+
+    $('#nextLevel').on('click', nextLevel);
+
+
+    //end of Gunman
+};
+
+
+var gunman1 = new Gunman(1),
+    gunman2 = new Gunman(2),
+    gunman3 = new Gunman(3),
+    gunman4 = new Gunman(4),
+    gunman5 = new Gunman(5);
+
+var allGunman = [gunman1, gunman2, gunman3, gunman4, gunman5];
+
+var gunman = allGunman[Math.floor(Math.random() * allGunman.length)];
+
+$('#gunman').addClass('gunman_defaul_position');
+
+function startGame() {
+    gunman.move();
+    gunman.stand();
+    gunman.fire();
 }
 
+$('#startGame').on('click', startGame);
 
-var Gunman = {
-    width: 0 + 'px',
-    height: 0 + 'px',
-    top: 0 + 'px',
-    left: 0 + 'px',
-    /*steps: function () {},*/
-    move: function () {
-        gunman.classList.add('gunman_walk');
-    },
-    standing: function () {
-        gunman.classList.remove('gunman_walk');
-        gunman.classList.add('gunman_standing');
-    },
-    shooting: function () {},
-    gunmanShootingTimer: function () {},
-    win: function () {},
-    lose: function () {}
+
+
+function nextLevel() {
+    gunman.move();
+    gunman.stand();
+    gunman.fire();
 }
 
-var player = {
-    playerShootingTime: function () {}
+$('#nextLevel').on('click', nextLevel);
+
+
+
+
+
+
+if (playerShoot == true) {
+    alert('hhh')
 }
-
-
-
-
-
-
-
-
-
-/*var game = {
-
-
-}
-
-
-
-
-
-var gunmanProperties = [{
-    width : 30,
-    height : 120,
-    top : 40,
-    left : 40,
-    stepsBeforeShoot : 3,
-    stepsAfter : 6,
-
-
-
-
-
-}]
-
-
-currentId = Math.round(Math.random()*5)
-gunman= gunmanProperties[currentId]
-
-$('.gunman').css({
-    'background-position': gunman.width + 'px',
-
-})
-
-
-gunman.step = function() {
-$('.gunman').css({
-    'background-position': (gunman.width + gunman.width) + 'px',
-
-}
- function go(stepsBeforeShoot) {
-for ()
-    step()
-
-}
-
-
-
-
-
-
-
-var player = {
-
-
-
-     }*/
--- markup/js_exam/notes/notes.js
+++ markup/js_exam/notes/notes.js
@@ -1,60 +1,114 @@
 var Game = {
     level: 0,
-    score: 0
+    score: 0,
+
+    Gunman: function (options) {
+
+        $('.button_restart').addClass('undisplay');
+        $('#nextLevel').addClass('undisplay');
+
+        var playerShoot = false;
+
+
+        function Timer() {
+            var timer = {
+                startTime: (new Date()).valueOf(),
+                currentTime: (new Date()).valueOf(),
+                inervalId: null,
+                interval: setInterval(function () {
+                    timer.currentTime = (new Date).valueOf();
+                }, 100)
+            };
+
+            function MyDateInterval(interval) {
+                var ret = {};
+                ret.interval = interval;
+                ret.s = Math.floor(interval / 1000) % 60 || 0;
+                ret.ms = (Math.floor(interval / 100));
+
+
+                return ret;
+            }
+
+
+            Object.observe(timer, function () {
+                var interval = new MyDateInterval(timer.currentTime - timer.startTime);
+                $('.player_timer').text(
+                    interval.s + ' : ' + interval.ms
+                );
+            });
+
+
+            setTimeout(function () {
+                /*WHAT IS THIS -->*/
+                clearInterval(timer.interval);
+            }, 16 * 100);
+        };
+
+        var currentGunman = options || 1;
+
+        this.move = function () {
+            $('#startGame').addClass('hide');
+            $('#credits').addClass('hide');
+            $('#gunman').addClass('gunman_' + currentGunman + '_walk');
+
+        };
+
+
+        this.stand = function () {
+            function gunmanStanding() {
+                $('#gunman').removeClass('gunman_' + currentGunman + '_walk');
+                $('#gunman').addClass('gunman_' + currentGunman + '_standing');
+            }
+            $('#gunman').on('transitionend webkitTransitionEnd oTransitionEnd', gunmanStanding, Timer);
+
+        };
+
+        function shoot() {
+            $("#gunman").on('click', function () {
+                $('#gunman').removeClass('gunman_' + currentGunman + '_standing');
+                $('#gunman').addClass('gunman_' + currentGunman + '_falling');
+                $('.message').addClass('hide');
+                playerShoot = true;
+
+            });
+        };
+
+
+        this.fire = function () {
+            setTimeout(shoot, 5000);
+            setTimeout(function () {
+                $('.message').removeClass('hide');
+            }, 5500);
+
+        };
+
+
+
+
+        //end of Gunman
+    },
+
+
+    gunman1: new Gunman(1),
+    gunman2: new Gunman(2),
+    gunman3: new Gunman(3),
+    gunman4: new Gunman(4),
+    gunman5: new Gunman(5),
+
+    allGunman: [gunman1, gunman2, gunman3, gunman4, gunman5],
+
+    gunman: allGunman[Math.floor(Math.random() * allGunman.length)],
+
+
+    startGame: function startGame() {
+        gunman.move();
+        gunman.stand();
+        gunman.fire();
+    },
+
+    start: function () {
+        $('#startGame').on('click', startGame.startGame);
+    }
+
 };
-
-var Gunman = function (options) {
-
-    var currentGunman = options || 1;
-
-    this.move = function () {
-        $('#startGame').addClass('hide');
-        $('#credits').addClass('hide');
-        $('#gunman').addClass('gunman_' + currentGunman + '_walk');
-
-    };
-    this.stand = function () {
-        function gunmanStanding() {
-            $('#gunman').removeClass('gunman_' + currentGunman + '_walk');
-            $('#gunman').addClass('gunman_' + currentGunman + '_standing');
-        }
-        $('#gunman').on('transitionend webkitTransitionEnd oTransitionEnd', gunmanStanding);
-    };
-
-    function shoot() {
-        function bang() {
-            $('#gunman').removeClass('gunman_' + currentGunman + '_standing');
-            $('#gunman').addClass('gunman_' + currentGunman + '_falling');
-        }
-        $("#gunman").click(bang);
-    };
-
-
-    this.fire = function () {
-        setTimeout(shoot, 5000);
-        setTimeout(function () {
-            $('.message').removeClass('hide');
-        }, 5500);
-
-    };
-
-};
-
-var gunman1 = new Gunman(1),
-    gunman2 = new Gunman(2),
-    gunman3 = new Gunman(3),
-    gunman4 = new Gunman(4),
-    gunman5 = new Gunman(5);
-
-var allGunman = [gunman1, gunman2, gunman3, gunman4, gunman5];
-
-var gunman = allGunman[Math.floor(Math.random() * allGunman.length)];
-
-
-function startGame() {
-    gunman.move();
-    gunman.stand();
-    gunman.fire();
-}
-
-$('#startGame').click(startGame);
